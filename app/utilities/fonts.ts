44a8247bab7ad015938ed223f01cbca0f7e55139
Jason <jasontwuk@gmail.com>
2024-10-01 17:26:10 +0100

    initial setup

---
+++ app/utilities/fonts.ts
@@ -0,0 +1,10 @@
+import { Roboto, Nunito, Ubuntu_Mono } from "next/font/google";
+
+export const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const nunito = Nunito({ subsets: ["latin"] });
+
+export const ubntu_mono = Ubuntu_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
