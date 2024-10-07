2f73314289247a209016a6fbb3a8a8bb1780bee6
Jason <jasontwuk@gmail.com>
2024-10-07 08:39:59 +0100

    add Dots component

---
+++ app/components/Dots.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+export const Dots = () => {
+  function addDot() {
+    // !!!!!!!! SELECT ITEMS !!!!!!!!
+    const heroContainer = document.getElementById("hero_container");
+    const dotsContainer = document.getElementById("dots_container");
+
+    // *** return when heroContainer or dotsContainer not exist
+    if (!heroContainer || !dotsContainer) return;
+
+    // !!!!!!!! VARIABLES !!!!!!!!
+    const dotAmount = 50;
+    const dotSize = 10;
+
+    for (let i = 0; i < dotAmount; i++) {
+      const newDot = document.createElement("div");
+      const size = Math.ceil(dotSize * Math.random());
+
+      newDot.classList.add("dot");
+
+      newDot.style.width = `${size}px`;
+      newDot.style.height = `${size}px`;
+      newDot.style.top = `${heroContainer.offsetHeight * Math.random()}px`;
+      newDot.style.left = `${heroContainer.clientWidth * Math.random()}px`;
+      // console.log(newDot);
+      dotsContainer.appendChild(newDot);
+    }
+  }
+
+  function dotBlink() {
+    // !!!!!!!! SELECT ITEMS !!!!!!!!
+    const dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < dots.length; i++) {
+      // *** make about 30% of the dots blink
+      const boolean = Math.random() * 10 > 7;
+
+      // *** remove previous blink status
+      dots[i].classList.remove("blink");
+
+      if (boolean) {
+        dots[i].classList.add("blink");
+      }
+    }
+  }
+
+  useEffect(() => {
+    addDot();
+
+    // *** dots blinking effect
+    const heroContainer = document.getElementById("hero_container");
+    let blinkInterval: ReturnType<typeof setInterval> | null = null;
+
+    const startBlinking = () => {
+      // console.log("mouse moving");
+
+      if (!blinkInterval) {
+        // console.log("start");
+        dotBlink();
+        blinkInterval = setInterval(dotBlink, 3000);
+      }
+    };
+
+    const stopBlinking = () => {
+      if (blinkInterval) {
+        // console.log("stop");
+        clearInterval(blinkInterval);
+        blinkInterval = null;
+      }
+    };
+
+    if (heroContainer) {
+      heroContainer.addEventListener("mouseover", startBlinking);
+      heroContainer.addEventListener("mouseleave", stopBlinking);
+    }
+
+    return () => {
+      if (heroContainer) {
+        heroContainer.removeEventListener("mouseover", startBlinking);
+        heroContainer.removeEventListener("mouseleave", stopBlinking);
+      }
+
+      // *** Clear interval when the component unmounts
+      stopBlinking();
+    };
+  }, []);
+
+  return (
+    <div
+      className="dots_container absolute left-0 top-0 -z-50"
+      id="dots_container"
+    ></div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Dots } from "./components/Dots";
 import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="relative" id="hero_container">
       <Hero />
+      <Dots />
     </div>
   );
 }
