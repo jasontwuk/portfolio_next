1254f58a0a3ddbf2938d09459825195f4ae16ae7
Jason <jasontwuk@gmail.com>
2024-10-07 18:55:27 +0100

    update media links in Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,15 +19,16 @@ export const Header = () => {
       <div className="flex items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-1 rounded-full border border-slate-300 p-1">
           {icons.map((icon) => (
-            <a
+            <Link
               href={icon.link}
               target="_blank"
               rel="noreferrer"
               key={icon.id}
-              className="rounded-full bg-slate-300 p-1.5 text-white duration-200 hover:bg-blue-600"
+              className="rounded-full bg-slate-300 p-1.5 text-base leading-4 text-white duration-200 hover:bg-blue-600"
+              aria-label={`${icon.name} link`}
             >
               <FontAwesomeIcon icon={icon.sign} className="h-4 w-4" />
-            </a>
+            </Link>
           ))}
         </div>
       </div>
