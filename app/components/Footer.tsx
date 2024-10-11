77c9d640f0f53dfdb6540b9c3cf44be35dffe320
Jason <jasontwuk@gmail.com>
2024-10-11 11:51:49 +0100

    move <Contact /> from page.tsx to Footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,18 +3,24 @@ import Link from "next/link";
 
 import { UnderlineText } from "./UnderlineText";
 
+import { Contact } from "./Contact";
+
 export const Footer = () => {
   return (
-    <footer className="flex flex-wrap items-center justify-center gap-2 text-center tracking-wide">
-      <span>
-        © 2020 - present&nbsp;
-        <Link href="/" className="group no-underline">
-          <UnderlineText>Jason Liao</UnderlineText>
-        </Link>
-        .
-      </span>
+    <footer className="flex flex-col items-center justify-center gap-20">
+      <Contact />
 
-      <span>All Rights Reserved.</span>
+      <div className="flex flex-wrap items-center justify-center gap-2 text-center tracking-wide">
+        <span>
+          © 2020 - present&nbsp;
+          <Link href="/" className="group no-underline">
+            <UnderlineText>Jason Liao</UnderlineText>
+          </Link>
+          .
+        </span>
+
+        <span>All Rights Reserved.</span>
+      </div>
     </footer>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { About } from "./components/About";
-import { Contact } from "./components/Contact";
 import { Dots } from "./components/Dots";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
@@ -15,8 +14,6 @@ export default function Home() {
       <About />
 
       <Projects />
-
-      <Contact />
     </div>
   );
 }
