7e1302b89700b16b82d7d519e07dd6759eecef6e
Jason <jasontwuk@gmail.com>
2024-10-12 17:38:17 +0100

    add ThemeSwitch & dark mode styles to Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import clsx from "clsx";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icons } from "../utilities/data";
 import { Logo } from "./Logo";
+import { ThemeSwitch } from "./ThemeSwitch";
 
 export const Header = () => {
   return (
@@ -16,20 +18,28 @@ export const Header = () => {
         <Logo />
       </Link>
 
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-1 rounded-full border border-slate-300 p-1">
-          {icons.map((icon) => (
-            <Link
-              href={icon.link}
-              target="_blank"
-              rel="noreferrer"
-              key={icon.id}
-              className="rounded-full bg-slate-300 p-1.5 text-base leading-4 text-white duration-200 hover:bg-blue-600"
-              aria-label={`${icon.name} link`}
-            >
-              <FontAwesomeIcon icon={icon.sign} className="block h-4 w-4" />
-            </Link>
-          ))}
+      <div className="flex items-center justify-center gap-4">
+        <ThemeSwitch />
+
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-1 rounded-full border border-slate-300 p-1">
+            {icons.map((icon) => (
+              <Link
+                href={icon.link}
+                target="_blank"
+                rel="noreferrer"
+                key={icon.id}
+                className={clsx(
+                  "rounded-full bg-slate-300 p-1.5 text-base leading-4 text-white duration-200 hover:bg-blue-600",
+                  // +++ Note: for dark mode
+                  "dark:bg-slate-400 dark:hover:bg-blue-600",
+                )}
+                aria-label={`${icon.name} link`}
+              >
+                <FontAwesomeIcon icon={icon.sign} className="block h-4 w-4" />
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </header>
