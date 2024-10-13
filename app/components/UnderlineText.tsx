0f64eed2ae4692dc9105187b5dcf4c31d99fb63e
Jason <jasontwuk@gmail.com>
2024-10-13 19:17:43 +0100

    fix a text break issue in UnderlineText

--- app/components/UnderlineText.tsx
+++ app/components/UnderlineText.tsx
@@ -13,7 +13,7 @@ export const UnderlineText = (props: UnderlineTextProps) => {
     <div className={clsx("inline-block tracking-wide", className)}>
       <span
         className={clsx(
-          "relative inline overflow-hidden text-blue-600",
+          "relative inline w-full overflow-hidden text-blue-600",
           // +++ Note: the hidden underline
           "after:absolute after:-bottom-0.5 after:left-0 after:z-[-1] after:h-px after:w-0 after:bg-blue-600 after:duration-200 after:content-['']",
           // +++ Note: show the hidden underline when hover
