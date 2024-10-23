2c6451debbd7cb2e3c96154e9ee62594ccfa10f0
Jason <jasontwuk@gmail.com>
2024-10-23 16:35:42 +0100

    fix a font class hydration issue

--- app/utilities/fonts.ts
+++ app/utilities/fonts.ts
@@ -3,11 +3,13 @@ import { Roboto, Nunito, Ubuntu_Mono } from "next/font/google";
 export const roboto = Roboto({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
+  display: "swap",
 });
 
-export const nunito = Nunito({ subsets: ["latin"] });
+export const nunito = Nunito({ subsets: ["latin"], display: "swap" });
 
 export const ubntu_mono = Ubuntu_Mono({
   subsets: ["latin"],
   weight: ["400", "700"],
+  display: "swap",
 });
